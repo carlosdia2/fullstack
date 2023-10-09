3cc316225a3e9fbd7c6e1153e53cb60b3dd2d6d7
carlosdia2 <carlosjavierdiazvilla@gmail.com>
2023-10-09 12:46:42 +0200

    hola

---
+++ proyectoInicio/proyectoJavascript/js/01-variables.js
@@ -0,0 +1,34 @@
+// alert("hola mundo")
+
+// creamos una variable dinamica, le damos el valor con = 
+
+let producto = "Nombre del producto";
+
+let disponible;
+
+
+disponible = true;
+
+disponible = "no hay producto";
+
+// let camiseta = "red";
+// let pantalon = "yellow";
+// let zapatos = "blue";
+
+let camiseta = "red",
+    pantalon = "yellow",
+    zapatos;
+
+    console.log("hola", camiseta);
+
+    zapatos= "negros";
+
+    console.log("color de camiseta:", camiseta);
+    console.log("color de pantalon:", pantalon);
+    console.log("color de zapatos:", zapatos);
+
+    let variable = true;
+
+    const variableConstante = "si";
+
+    console.log("constante variable: ", variableConstante);
---
+++ proyectoInicio/proyectoJavascript/js/02-tipos-de-datos.js
@@ -0,0 +1,35 @@
+const producto1 = "Juego de mesa EL PUEBLO DUERME"; //(es la forma mas utilizada)
+const producto2 = String("1");
+const producto3 = new String("UNO");  //es para crear un objeto, lo veremos luego.
+
+console.log(typeof producto1, producto1);
+console.log(typeof producto2);
+console.log(typeof producto3);
+
+// arriba tenemos tres formas de declarar variables de texto (String).
+
+
+let numero100;
+let numero200;
+
+numero100 = false;
+numero200 = "200";
+
+console.log(typeof numero100);
+console.log(typeof numero200);
+
+console.log("METODO LENGTH:", producto1.length); 
+
+console.log("Metodo indexOf:",producto1.indexOf("DUERME"));
+
+console.log("Metodo indexOf:",producto1.includes("DUERME"));
+
+crear 3 variables de tipo string e imprimir en consola las tres usando length, indexOf y 
+
+let string1 = "hola";
+let string2 = "hola2";
+let string3 = "hola2";
+
+console.log("la variable 1 tiene:", string1.length, "caracteres.");
+console.log("la l de 'hola' está en la posicion:", string2.indexOf("l"));
+console.log("¿está la palabra 'chao' en la variable string3?", string3.includes("chao"));
